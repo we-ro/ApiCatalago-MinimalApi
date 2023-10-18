c22e53ac7c65811f4f66d65def38202f65a110b6
we-ro <wellington.rob@outlook.com.br>
2023-10-18 19:25:37 -0300

    Adicionar arquivos de projeto.

---
+++ ApiCatalago/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using ApiCatalago.Models;
+
+
+namespace ApiCatalogo.Services;
+
+public interface ITokenService
+{
+    string GerarToken(string key, string issuer, string audience, UserModel user);
+}
